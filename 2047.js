9535c7ad55315eb4a4b69673ef174d6c21e7a028
WetWipee <48190204+WetWipee@users.noreply.github.com>
2021-11-02 08:08:53 -0500

    r works now

--- 2047.js
+++ 2047.js
@@ -10,13 +10,21 @@ let innersquarequad = ((1 - borderScale) * canvasWidth) / 4;
 // arrays
 let grid = [];
 let antigrid = [];
+let antiantigrid = [];
 let tile1 = [];
 let ranAntigrid;
 let lost = "FALSE";
 
+function clearArray() {
+  grid = [];
+  antigrid = [];
+  antiantigrid = [];
+  tile1 = [];
+}
+
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
-  frameRate(60);
+  frameRate(24);
   boardVectors();
   noLoop();
   console.log(antigrid)
@@ -94,36 +102,70 @@ function RandomizeAntiGrid() {
     text("you lose LLL", canvasWidth/3, canvasWidth/2)
     lost = "TRUE"
   } else {
-  ranAntigrid = round(random(0, antigrid.length - 1));
-  console.log(ranAntigrid);
-  console.log(antigrid[ranAntigrid])
-}
+    ranAntigrid = round(random(0, antigrid.length - 1));
+    console.log(ranAntigrid);
+    console.log(antigrid[ranAntigrid])
+  }
 }
 
 function draw1() {
   if (lost === "FALSE") {
-  RandomizeAntiGrid();
+    RandomizeAntiGrid();
 
-  console.log("current antigrid length " + antigrid.length);
+    console.log("current antigrid length " + antigrid.length);
 
-  tile1.push(new p5.Vector(antigrid[ranAntigrid].x, antigrid[ranAntigrid].y, antigrid[ranAntigrid].z));
+    tile1.push(new p5.Vector(antigrid[ranAntigrid].x, antigrid[ranAntigrid].y, antigrid[ranAntigrid].z));
 
-  for (let i = 0; i < tile1.length; i++) {
-    fill("#ede5da");
-    rect(tile1[i].x, tile1[i].y, tile1[i].z, tile1[i].z, 3.8);
-    fill("#776e64");
-    text("1", tile1[i].x + tile1[i].z / 2, tile1[i].y + tile1[i].z / 2);
+    for (i = 0; i < tile1.length; i++) {
+      console.log("tile1's " + tile1[i])
+    }
+
+    for (let i = 0; i < tile1.length; i++) {
+      fill("#ede5da");
+      rect(tile1[i].x, tile1[i].y, tile1[i].z, tile1[i].z, 3.8); // drawing 1
+      fill("#776e64");
+      textSize(16)
+      text("1", tile1[i].x + tile1[i].z / 2, tile1[i].y + tile1[i].z / 2);
+    }
+    // tile1.pop();
+
+    antiantigrid.push(new p5.Vector(antigrid[ranAntigrid].x, antigrid[ranAntigrid].y, antigrid[ranAntigrid].z))
+
+    antigrid.splice(ranAntigrid, 1)
+    if (antigrid.length === 0) {
+      RandomizeAntiGrid()
+    }
+  } else if (lost === "TRUE") {
+    tile1 = []
+    for (let i = 0; i < 16; i++) {
+      antigrid.push(
+        new p5.Vector(
+          grid[i].x,
+          grid[i].y,
+          grid[i].z
+        )
+      )
+      lost = "FALSE"
+    }
   }
-  //tile1.pop();
-  antigrid.splice(ranAntigrid, 1)
-  if (antigrid.length === 0) {
-    RandomizeAntiGrid()
-  }
-}
 }
 
 function mousePressed() {
   if (lost === "FALSE") {
-  redraw();
+    redraw();
+  }
 }
+
+function keyPressed() {
+  if (keyCode === 82) {
+    // has to do it twice cuz of loss detection idfk what im doing
+    clear()
+    clearArray()
+    setup()
+    redraw()
+    clear()
+    clearArray()
+    setup()
+    redraw()
+  }
 }
